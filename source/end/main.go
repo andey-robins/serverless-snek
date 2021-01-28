ab36f34316329b79ad2c07af2f78289af8c623fa
Andey Robins <andeyrobins@gmail.com>
2021-01-28 16:54:54 -0700

    Finish end

--- source/end/main.go
+++ source/end/main.go
@@ -15,10 +15,12 @@ type Response events.APIGatewayProxyResponse
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context) (Response, error) {
+
+	// this one, just like start, we ignore because there is no state to deconstruct/deallocate at the end of a game
 	resp := Response{
 		StatusCode:      200,
 		IsBase64Encoded: false,
-		Body:            "Golang Serverless Battlesnake",
+		Body:            "And Make it Double",
 		Headers: map[string]string{
 			"Content-Type":           "text/plain",
 			"X-MyCompany-Func-Reply": "hello-handler",
