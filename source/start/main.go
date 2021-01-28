542c6381c7c953ee0cce6013f2b7e60b466bc61c
Andey Robins <andeyrobins@gmail.com>
2021-01-28 16:53:41 -0700

    Finish start

--- source/start/main.go
+++ source/start/main.go
@@ -15,10 +15,12 @@ type Response events.APIGatewayProxyResponse
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context) (Response, error) {
+
+	// we do nothing here beause the challenge is to build a state-less snake. this one doesn't track anything as a result and so we don't set up anything when we receive a start message
 	resp := Response{
 		StatusCode:      200,
 		IsBase64Encoded: false,
-		Body:            "Golang Serverless Battlesnake",
+		Body:            "Prepare for Trouble",
 		Headers: map[string]string{
 			"Content-Type":           "text/plain",
 			"X-MyCompany-Func-Reply": "hello-handler",
